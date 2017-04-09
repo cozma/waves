9ae69bee5e6384302fb9f834a67ae1ac9d395aca
Ross <maslar.5@osu.edu>
2017-04-08 22:09:55 -0400

    WIP

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var env = new Primrose.BrowserEnvironment({
-  useFog: true,
-  groundTexture: 0x7f7f7f,
-  backgroundColor: 0xffffff,
-  fullScreenButtonContainer: "#fullScreenButtonContainer"
-});
+$(document).ready(function() {
+  $.getScript("scripts/preloader.js");
+  $.getScript("scripts/three.js").done(function(script, textStatus) {
+    $.getScript("scripts/Site.js").done(function(script, textStatus) {
+      $.getScript("scripts/Primrose.js").done(function(script, textStatus) {
+        var env = new Primrose.BrowserEnvironment({
+          useFog: true,
+          groundTexture: 0x7f7f7f,
+          backgroundColor: 0xffffff,
+          fullScreenButtonContainer: "#fullScreenButtonContainer"
+        });
 
-
-env.on("ready", function() {
-
-  camera(1)
-    .then(function(cam) {
-
-      cam.play();
-      cam.addTo(env.head)
-        .at(0, 0, -0.5);
-
-      Preloader.hide();
+        env.on("ready", function() {
+          camera(0)
+          //cam.play();
+          //cam.addTo(env.head).at(0, 0, -0.5);
+          Preloader.hide();
+        });
+      });
     });
-
+  });
 });
